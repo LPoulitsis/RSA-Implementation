0d255ab956f4d69827d44915c6adb149c35ab7a6
Lefteris Poulitsis <l.poulitsis@gmail.com>
2024-07-11 22:31:42 +0300

    Fix redefinition issues.

--- src/headers/rsa.hh
+++ src/headers/rsa.hh
@@ -1,12 +1,38 @@
+#pragma once
+
+#include <cmath>
 #include <iostream>
+#include <random>
+#include <string>
 
 using namespace std;
+class PublicKey {
+   public:
+    void SetE(int e);
+    void SetN(int N);
 
-// A small set of prime numbers
-int primeNumbers[] = {2,  3,  5,  7,  11, 13, 17, 19, 23, 29, 31, 37, 41,
-                      43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101};
+    int GetE();
+    int GetN();
 
-int p, q;
+   private:
+    // Public key consists of (N, e)
+    int N;
+    int e;
+};
+
+class PrivateKey {
+   public:
+    void SetD(int d);
+    void SetN(int N);
+
+    int GetD();
+    int GetN();
+
+   private:
+    // Private key consists of (N, d)
+    int N;
+    int d;
+};
 
 class Rsa {
    public:
@@ -39,32 +65,4 @@ class Rsa {
 
     // Calculates Φ(Ν) = N - p - q + 1 and returns the outcome
     int Phi(int N);
-};
-
-class PublicKey {
-   public:
-    void SetE(int e);
-    void SetN(int N);
-
-    int GetE();
-    int GetN();
-
-   private:
-    // Public key consists of (N, e)
-    int N;
-    int e;
-};
-
-class PrivateKey {
-   public:
-    void SetD(int d);
-    void SetN(int N);
-
-    int GetD();
-    int GetN();
-
-   private:
-    // Private key consists of (N, d)
-    int N;
-    int d;
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,3 @@
-#include <iostream>
-
 #include "headers/user.hh"
 
 int main() {
